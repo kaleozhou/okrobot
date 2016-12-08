a31e083c53a2c5f3d801ed22c34a2a554c053f76
kaleo <kaleo1990@hotmail.com>
2016-12-08 20:34:30 +0800

    base_rate

--- phpcms/modules/okrobot/index.php
+++ phpcms/modules/okrobot/index.php
@@ -17,21 +17,7 @@ class index {
     }
     public function init() 
     {
-        // 必须加载扩展  
-     //   if (!function_exists("pcntl_fork")) {  
-     //       die("pcntl extention is must !");  
-     //   }  
-     //   $pid = pcntl_fork();    //创建子进程  
-     //   //父进程和子进程都会执行下面代码  
-     //   if ($pid == -1) {  
-     //       //错误处理：创建子进程失败时返回-1.  
-     //       die('could not fork');  
-     //   } else if ($pid) {  
-            //父进程会得到子进程号，所以这里是父进程执行的逻辑  
-            //如果不需要阻塞进程，而又想得到子进程的退出状态，则可以注释掉pcntl_wait($status)语句，或写成：  
-     //       pcntl_wait($status,WNOHANG); //等待子进程中断，防止子进程成为僵尸进程。  
             $res=refresh_userinfo();
-            // include template('okrobot', 'index');
             $res=autotrade();
             //取得用户信息
             $data="asset_net,asset_total,free_cny,free_btc";
@@ -44,15 +30,11 @@ class index {
             //取得行情信息
             $newticker=$this->ticker_db->get_one('','*','id desc');
             $str=$str."price:".$newticker['lastprice']."|";
+            $str=$str.$newticker['base_rate']."|";
             //取得设置信息
             $newset=$this->set_db->get_one('','*','id desc');
             $str=$str."baseprice:".$newset['base_price']."|";
             $str=$str."\n";
             printf($str);
-     //   } else {  
-     //       //子进程得到的$pid为0, 所以这里是子进程执行的逻辑。  
-     //       $res=refresh_userinfo();
-     //       exit(0) ;  
-     //   }  
     }
 }
