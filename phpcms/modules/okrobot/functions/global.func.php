8839a4df20985f963e23a326f2ddf217a9bcd31f
kaleo <kaleo1990@hotmail.com>
2016-12-10 11:14:22 +0800

    trade

--- phpcms/modules/okrobot/functions/global.func.php
+++ phpcms/modules/okrobot/functions/global.func.php
@@ -151,10 +151,10 @@ function autotrade(){
                         $trade['symbol']=$symbol;
                         $trade['tradetype']=$tradetype;
                         $trade['result']=strval($result->result);
-                   //     if ($result['result']=='1')
-                   //     {
-                   //        // $trade['order_id']=$result->order_id;
-                   //     }
+                        if ($result['result']=='1')
+                        {
+                            $trade['order_id']=$result->order_id;
+                        }
                         $trade_db->insert($trade,true);
                     }
                     else
@@ -169,6 +169,10 @@ function autotrade(){
                         $trade['symbol']=$symbol;
                         $trade['tradetype']=$tradetype;
                         $trade['result']=strval($result->result);
+                        if ($result['result']=='1')
+                        {
+                            $trade['order_id']=$result->order_id;
+                        }
                         $trade_db->insert($trade,true);
 
                     }
@@ -197,6 +201,10 @@ function autotrade(){
                         $trade['symbol']=$symbol;
                         $trade['tradetype']=$tradetype;
                         $trade['result']=strval($result->result);
+                        if ($result['result']=='1')
+                        {
+                            $trade['order_id']=$result->order_id;
+                        }
                         $trade_db->insert($trade,true);
                     }
                 }
