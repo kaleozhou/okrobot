32507fbdc91eb87b52b63b1257ee540e01f99aaa
kaleo <kaleo1990@hotmail.com>
2016-12-12 20:26:32 +0800

    chonggou

--- phpcms/modules/okrobot/functions/global.func.php
+++ phpcms/modules/okrobot/functions/global.func.php
@@ -18,21 +18,70 @@ function api_to_database($tablename){
         //获取用户信息
         $params = array('api_key' => API_KEY);
         $result = $client -> userinfoApi($params);
-        $userinfo=object_userinfo($result);
+        //todatabase
+        $userinfo=array();
+        $userinfo['asset_net']=$result->info->funds->asset->net;
+        $userinfo['asset_total']=$result->info->funds->asset->total;
+        $userinfo['borrow_btc']=$result->info->funds->borrow->btc;
+        $userinfo['borrow_cny']=$result->info->funds->borrow->cny;
+        $userinfo['borrow_ltc']=$result->info->funds->borrow->ltc;
+        $userinfo['free_btc']=$result->info->funds->free->btc;
+        $userinfo['free_cny']=$result->info->funds->free->cny;
+        $userinfo['free_ltc']=$result->info->funds->free->ltc;
+        $userinfo['freezed_btc']=$result->info->funds->freezed->btc;
+        $userinfo['freezed_cny']=$result->info->funds->freezed->cny;
+        $userinfo['freezed_ltc']=$result->info->funds->freezed->ltc;
+        $userinfo['union_fund']=date("Y/m/d H:i:s");
         $userinfo_db->insert($userinfo,true);
         break;
     case 'ticker':
         //获取OKCoin行情（盘口数据）
         $params = array('symbol' => 'btc_cny');
         $result = $client -> tickerApi($params);
-        $ticker=object_ticker($result);
+        //todatabase
+        $ticker=array();
+        $ticker['buy']=$result->ticker->buy;
+        $ticker['high']=$result->ticker->high;
+        $ticker['last_price']=$result->ticker->last;
+        $ticker['low']=$result->ticker->low;
+        $ticker['sell']=$result->ticker->sell;
+        $ticker['vol']=$result->ticker->vol;
+        //计算偏移率
+        $newset=get_new_info('set');
+        $last_price=$ticker['last_price'];
+        $my_last_price=$newset['my_last_price'];
+        $dif_price=$last_price-$my_last_price;
+        $ticker['dif_price']=$dif_price;
+        if($my_last_price!=0){
+            $base_rate=$dif_price/$my_last_price;
+        }
+        $ticker['base_rate']=$base_rate;
+        $ticker['tickerdate']=date("Y/m/d H:i:s");
         $res=$ticker_db->insert($ticker,true);
         break;
     case 'orderinfo':
         //批量获取用户订单
         $params = array('api_key' => API_KEY, 'symbol' => 'btc_cny', 'status' => 1, 'current_page' => '1', 'page_length' => '15');
         $result = $client -> orderHistoryApi($params);
-        $orders=object_orderinfo($result);
+        $orderinfo=array();
+        $orders=array();
+        $ordersresult=$result->orders;
+        $i=0;
+        foreach ($ordersresult as $key) {
+            //取出api结果
+            $orderinfo['amount']=$key->amount;  
+            $orderinfo['deal_amount']=$key->deal_amount;  
+            $orderinfo['avg_price']=$key->avg_price;
+            //由于获取的是java时间戳，去掉后三位  
+            $orderinfo['create_date']=date("Y/m/d H:i:s",substr($key->create_date,0,strlen($key->create_date)-3));  
+            $orderinfo['order_id']=$key->order_id;  
+            $orderinfo['price']=$key->price;  
+            $orderinfo['status']=$key->status;  
+            $orderinfo['symbol']=$key->symbol;  
+            $orderinfo['ordertype']=$key->type;  
+            $orders[$i]=$orderinfo;
+            $i++;
+        }
         //将订单插入数据库
         if(count($orders)>0){
             foreach ($orders as $key) {
@@ -59,7 +108,21 @@ function api_to_database($tablename){
         $type=KLINETYPE;
         $params = array('symbol' => 'btc_cny', 'type' =>$type, 'size' => 20);
         $result = $client -> klineDataApi($params);
-        $klines=object_kline($result);
+        $klines=array();
+        $kline=array();
+        $i=0;
+        foreach ($result as $reskline) {
+            //取出每个kline
+            $kline['create_date']=date("Y/m/d H:i:s",substr($reskline[0],0,strlen($reskline[0])-3));  
+            $kline['start_price']=$reskline[1];
+            $kline['high_price']=$reskline[2];
+            $kline['low_price']=$reskline[3];
+            $kline['over_price']=$reskline[4];
+            $kline['vol']=$reskline[5];
+            $kline['dif_price']=$kline['high_price']-$kline['low_price'];
+            $klines[$i]=$kline;
+            $i++;
+        }
         if (count($klines)>0) {
             foreach ($klines as $key) {
                 //如果时间戳相等，不插入否则插入
@@ -91,23 +154,25 @@ function api_to_database($tablename){
         $set_db->insert($set,true);
         break;
     case 'borrow':
-	$params = array('api_key' => API_KEY, 'symbol' => 'btc_cny');
-    $result = $client -> borrowsInfoApi($params);
-    $borrow=array();
-    $borrow['borrow_btc']=$result->borrow_btc;
-    $borrow['borrow_cny']=$result->borrow_cny;
-    $borrow['borrow_ltc']=$result->borrow_ltc;
-    $borrow['can_borrow']=$result->can_borrow;
-    $borrow['interest_btc']=$result->interest_btc;
-    $borrow['interest_cny']=$result->interest_cny;
-    $borrow['interest_ltc']=$result->interest_ltc;
-    $borrow['today_interest_btc']=$result->today_interest_btc;
-    $borrow['today_interest_ltc']=$result->today_interest_ltc;
-    $borrow['today_interest_cny']=$result->today_interest_cny;
-    $borrow['result']=$result->result;
-    $borrow['create_date']=date('Y/m/d H:i:s');
-    $borrow_db->insert($borrow,true);	
-            break;
+        //api
+        $params = array('api_key' => API_KEY, 'symbol' => 'btc_cny');
+        $result = $client -> borrowsInfoApi($params);
+        //todatabase
+        $borrow=array();
+        $borrow['borrow_btc']=$result->borrow_btc;
+        $borrow['borrow_cny']=$result->borrow_cny;
+        $borrow['borrow_ltc']=$result->borrow_ltc;
+        $borrow['can_borrow']=$result->can_borrow;
+        $borrow['interest_btc']=$result->interest_btc;
+        $borrow['interest_cny']=$result->interest_cny;
+        $borrow['interest_ltc']=$result->interest_ltc;
+        $borrow['today_interest_btc']=$result->today_interest_btc;
+        $borrow['today_interest_ltc']=$result->today_interest_ltc;
+        $borrow['today_interest_cny']=$result->today_interest_cny;
+        $borrow['result']=$result->result;
+        $borrow['create_date']=date('Y/m/d H:i:s');
+        $borrow_db->insert($borrow,true);	
+        break;
     default:
         break;
     }
@@ -172,94 +237,6 @@ function get_new_info($tablename){
         break;
     }
 }
-//将获取的数据转换成userinfo数据
-function object_userinfo($result){
-    $userinfo=array();
-    $userinfo['asset_net']=$result->info->funds->asset->net;
-    $userinfo['asset_total']=$result->info->funds->asset->total;
-    $userinfo['borrow_btc']=$result->info->funds->borrow->btc;
-    $userinfo['borrow_cny']=$result->info->funds->borrow->cny;
-    $userinfo['borrow_ltc']=$result->info->funds->borrow->ltc;
-    $userinfo['free_btc']=$result->info->funds->free->btc;
-    $userinfo['free_cny']=$result->info->funds->free->cny;
-    $userinfo['free_ltc']=$result->info->funds->free->ltc;
-    $userinfo['freezed_btc']=$result->info->funds->freezed->btc;
-    $userinfo['freezed_cny']=$result->info->funds->freezed->cny;
-    $userinfo['freezed_ltc']=$result->info->funds->freezed->ltc;
-    $userinfo['union_fund']=date("Y/m/d H:i:s");
-    return $userinfo;
-} 
-//将数据转换成ticker数据
-function object_ticker($result){
-    //加载model
-    $userinfo_db=pc_base::load_model('okrobot_userinfo_model');
-    $ticker_db=pc_base::load_model('okrobot_ticker_model');
-    $orderinfo_db=pc_base::load_model('okrobot_orderinfo_model');
-    $trade_db=pc_base::load_model('okrobot_trade_model');
-    $set_db=pc_base::load_model('okrobot_set_model');
-    $kline_db=pc_base::load_model('okrobot_kline_model');
-    $ticker=array();
-    $ticker['buy']=$result->ticker->buy;
-    $ticker['high']=$result->ticker->high;
-    $ticker['last_price']=$result->ticker->last;
-    $ticker['low']=$result->ticker->low;
-    $ticker['sell']=$result->ticker->sell;
-    $ticker['vol']=$result->ticker->vol;
-    //计算偏移率
-    $newset=get_new_info('set');
-    $last_price=$ticker['last_price'];
-    $my_last_price=$newset['my_last_price'];
-    $dif_price=$last_price-$my_last_price;
-    $ticker['dif_price']=$dif_price;
-    if($my_last_price!=0){
-        $base_rate=$dif_price/$my_last_price;
-    }
-    $ticker['base_rate']=$base_rate;
-    $ticker['tickerdate']=date("Y/m/d H:i:s");
-    return $ticker;
-}
-//将数据转换成orderinfo数据
-function object_orderinfo($result){
-    $orderinfo=array();
-    $orders=array();
-    $ordersresult=$result->orders;
-    $i=0;
-    foreach ($ordersresult as $key) {
-        //取出api结果
-        $orderinfo['amount']=$key->amount;  
-        $orderinfo['deal_amount']=$key->deal_amount;  
-        $orderinfo['avg_price']=$key->avg_price;
-        //由于获取的是java时间戳，去掉后三位  
-        $orderinfo['create_date']=date("Y/m/d H:i:s",substr($key->create_date,0,strlen($key->create_date)-3));  
-        $orderinfo['order_id']=$key->order_id;  
-        $orderinfo['price']=$key->price;  
-        $orderinfo['status']=$key->status;  
-        $orderinfo['symbol']=$key->symbol;  
-        $orderinfo['ordertype']=$key->type;  
-        $orders[$i]=$orderinfo;
-        $i++;
-    }
-    return $orders;
-}
-//将数据转换成kline数据
-function object_kline($result){
-    $klines=array();
-    $kline=array();
-    $i=0;
-    foreach ($result as $reskline) {
-        //取出每个kline
-        $kline['create_date']=date("Y/m/d H:i:s",substr($reskline[0],0,strlen($reskline[0])-3));  
-        $kline['start_price']=$reskline[1];
-        $kline['high_price']=$reskline[2];
-        $kline['low_price']=$reskline[3];
-        $kline['over_price']=$reskline[4];
-        $kline['vol']=$reskline[5];
-        $kline['dif_price']=$kline['high_price']-$kline['low_price'];
-        $klines[$i]=$kline;
-        $i++;
-    }
-    return $klines;
-}
 //发送通知短信
 function send_sms($content){
     $sms_db=pc_base::load_model('okrobot_sms_model');
@@ -289,7 +266,7 @@ function send_sms($content){
     $newsms['sms_type']=$sms_type;
     $sms_db->insert($newsms,true);
 }
-//下单函数
+//自动下单函数
 function autotrade(){
     try{
         //加载model
